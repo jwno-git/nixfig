a743d8aee71531899d1dba23e29bb59f444298b0
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-07-01 22:44:46 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -163,7 +163,7 @@
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
 	#Apps
-	brave
+	google-chrome
 	eog
 	firefox
 	gimp
